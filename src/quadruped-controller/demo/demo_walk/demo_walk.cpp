b5a0d05d7d20de02f96e5d41197c661e2c4a88d6
Sherman <1973671061@qq.com>
2022-06-15 01:52:16 -0700

    adjusted structure

--- src/quadruped-controller/demo/demo_walk/demo_walk.cpp
+++
@@ -1,9 +0,0 @@
-#include <ros/ros.h>
-
-int main(int argc, char **argv)
-{
-  ros::init(argc, argv, "demo_walk");
-  ros::NodeHandle nh;
-
-  ROS_INFO("Hello world!");
-}
--- src/quadruped-controller/test/test_node.cpp
+++
@@ -1,9 +0,0 @@
-#include <ros/ros.h>
-
-int main(int argc, char **argv)
-{
-  ros::init(argc, argv, "test_node");
-  ros::NodeHandle nh;
-
-  ROS_INFO("Hello world!");
-}
