f6fef0ff6fb20096048e2b34bf91ac113f7fe962
Sherman <1973671061@qq.com>
2022-06-25 01:27:07 -0700

    edit namespace in qr_algebra

--- src/quadruped/include/common/qr_algebra.h
+++ src/quadruped/include/common/qr_algebra.h
@@ -30,92 +30,88 @@
 
 #include "qr_sparse_matrix.hpp"
 
-namespace robotics {
-    namespace math {
+namespace Math {
 
-        /** @brief Square a number */
-        template<typename T>
-        T square(T a)
-        {
-            return a * a;
+    /** @brief Square a number */
+    template<typename T>
+    T square(T a)
+    {
+        return a * a;
+    }
+
+    /** @brief Are two eigen matrices almost equal? */
+    template<typename T, typename T2>
+    bool almostEqual(const Eigen::MatrixBase<T> &a, const Eigen::MatrixBase<T> &b, T2 tol)
+    {
+        long x = T::RowsAtCompileTime;
+        long y = T::ColsAtCompileTime;
+
+        if (T::RowsAtCompileTime == Eigen::Dynamic ||
+            T::ColsAtCompileTime == Eigen::Dynamic) {
+            assert(a.rows() == b.rows());
+            assert(a.cols() == b.cols());
+            x = a.rows();
+            y = a.cols();
         }
 
-        /** @brief Are two eigen matrices almost equal? */
-        template<typename T, typename T2>
-        bool almostEqual(const Eigen::MatrixBase<T> &a, const Eigen::MatrixBase<T> &b, T2 tol)
-        {
-            long x = T::RowsAtCompileTime;
-            long y = T::ColsAtCompileTime;
-
-            if (T::RowsAtCompileTime == Eigen::Dynamic ||
-                T::ColsAtCompileTime == Eigen::Dynamic) {
-                assert(a.rows() == b.rows());
-                assert(a.cols() == b.cols());
-                x = a.rows();
-                y = a.cols();
+        for (long i = 0; i < x; i++) {
+            for (long j = 0; j < y; j++) {
+                T2 error = std::abs(a(i, j) - b(i, j));
+                if (error >= tol) return false;
             }
+        }
+        return true;
+    }
 
-            for (long i = 0; i < x; i++) {
-                for (long j = 0; j < y; j++) {
-                    T2 error = std::abs(a(i, j) - b(i, j));
-                    if (error >= tol) return false;
-                }
+    /** @brief Are two float type number almost equal? */
+    template<typename T>
+    bool almostEqual(const T &a, const T b, T tol)
+    {
+        T error = std::abs(a - b);
+        if (error >= tol) {
+            return false;
+        }
+        return true;
+    }
+
+    /*!
+    * Compute the pseudo inverse of a matrix
+    * @param matrix : input matrix
+    * @param sigmaThreshold : threshold for singular values being zero
+    * @param invMatrix : output matrix
+    */
+    template <typename T>
+    void pseudoInverse(DMat<T> const& matrix, double sigmaThreshold,
+                    DMat<T>& invMatrix)
+    {
+        if ((1 == matrix.rows()) && (1 == matrix.cols())) {
+            invMatrix.resize(1, 1);
+            if (matrix.coeff(0, 0) > sigmaThreshold) {
+                invMatrix.coeffRef(0, 0) = 1.0 / matrix.coeff(0, 0);
+            } else {
+                invMatrix.coeffRef(0, 0) = 0.0;
             }
-            return true;
+            return;
         }
 
-        /** @brief Are two float type number almost equal? */
-        template<typename T>
-        bool almostEqual(const T &a, const T b, T tol)
-        {
-            T error = std::abs(a - b);
-            if (error >= tol) {
-                return false;
+        Eigen::JacobiSVD<DMat<T>> svd(matrix,
+                                        Eigen::ComputeThinU | Eigen::ComputeThinV);
+        // not sure if we need to svd.sort()... probably not
+        int const nrows(svd.singularValues().rows());
+        DMat<T> invS;
+        invS = DMat<T>::Zero(nrows, nrows);
+        for (int ii(0); ii < nrows; ++ii) {
+            if (svd.singularValues().coeff(ii) > sigmaThreshold) {
+                invS.coeffRef(ii, ii) = 1.0 / svd.singularValues().coeff(ii);
+            } else {
+                // invS.coeffRef(ii, ii) = 1.0/ sigmaThreshold;
+                // printf("sigular value is too small: %f\n",
+                // svd.singularValues().coeff(ii));
             }
-            return true;
         }
-
-        /*!
-        * Compute the pseudo inverse of a matrix
-        * @param matrix : input matrix
-        * @param sigmaThreshold : threshold for singular values being zero
-        * @param invMatrix : output matrix
-        */
-        template <typename T>
-        void pseudoInverse(DMat<T> const& matrix, double sigmaThreshold,
-                        DMat<T>& invMatrix) 
-        {
-            if ((1 == matrix.rows()) && (1 == matrix.cols())) {
-                invMatrix.resize(1, 1);
-                if (matrix.coeff(0, 0) > sigmaThreshold) {
-                    invMatrix.coeffRef(0, 0) = 1.0 / matrix.coeff(0, 0);
-                } else {
-                    invMatrix.coeffRef(0, 0) = 0.0;
-                }
-                return;
-            }
-
-            Eigen::JacobiSVD<DMat<T>> svd(matrix,
-                                            Eigen::ComputeThinU | Eigen::ComputeThinV);
-            // not sure if we need to svd.sort()... probably not
-            int const nrows(svd.singularValues().rows());
-            DMat<T> invS;
-            invS = DMat<T>::Zero(nrows, nrows);
-            for (int ii(0); ii < nrows; ++ii) {
-                if (svd.singularValues().coeff(ii) > sigmaThreshold) {
-                    invS.coeffRef(ii, ii) = 1.0 / svd.singularValues().coeff(ii);
-                } else {
-                    // invS.coeffRef(ii, ii) = 1.0/ sigmaThreshold;
-                    // printf("sigular value is too small: %f\n",
-                    // svd.singularValues().coeff(ii));
-                }
-            }
-            invMatrix = svd.matrixV() * invS * svd.matrixU().transpose();
-        }
-
-
-    } //  namespace math
-} // namespace robotics
+        invMatrix = svd.matrixV() * invS * svd.matrixU().transpose();
+    }
+} //  namespace math
 
 template<typename T>
 using DenseMatrix = Eigen::Matrix<T, Eigen::Dynamic, Eigen::Dynamic>;
