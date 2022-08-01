d6490938985b723e0f33705e76cc9b62921dd67d
Gong, Ke <gk.kingj@gmail.com>
2022-08-01 23:48:01 +0800

    add controller comments

--- src/quadruped/include/quadruped/mpc_controller/qr_raibert_swing_leg_controller.h
+++ src/quadruped/include/quadruped/mpc_controller/qr_raibert_swing_leg_controller.h
@@ -1,14 +1,29 @@
-/*
-* Copyright (c) Huawei Technologies Co., Ltd. 2021-2022. All rights reserved.
-* Description: Swing Leg Controller
-* Author: Xie Ming Cheng & Zhao Yao
-* Create: 2021-10-25
-* Notes: xx
-* Modify: add head comment and add some function comments and delete some test functions. @ xie_mingcheng 2021.11.22
-*/
+// The MIT License
 
-#ifndef ASCEND_QUADRUPED_CPP_RAIBERT_SWING_LEG_CONTROLLER_H
-#define ASCEND_QUADRUPED_CPP_RAIBERT_SWING_LEG_CONTROLLER_H
+// Copyright (c) 2022 
+// Robot Motion and Vision Laboratory at East China Normal University
+// Contact: tophill.robotics@gmail.com
+
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE. 
+
+#ifndef QR_SWING_LEG_CONTROLLER_H
+#define QR_SWING_LEG_CONTROLLER_H
 
 #include "robots/qr_motor.h"
 #include "qr_gait_generator.h"
@@ -21,6 +36,19 @@ namespace Quadruped {
     class qrSwingLegController {
 
     public:
+
+        /**
+         * @brief Construct a qrSwingLegController object using the given parameters.
+         * @param robot The robot to which the controller is associated.
+         * @param gaitGenerator The gait generator 
+         * @param stateEstimator The gait estimator
+         * @param groundEstimator The ground estimator
+         * @param FootholdPlanner The foothold planner
+         * @param desiredLinearSpeed The desired linear velocity
+         * @param desiredTwistingSpeed The desired angular velocity
+         * @param footClearance 
+         * @param configPath The config file path
+         */
         qrSwingLegController(qrRobot *robot,
                                   qrGaitGenerator *gaitGenerator,
                                   qrRobotEstimator *stateEstimator,
@@ -32,14 +60,17 @@ namespace Quadruped {
                                   float footClearance,
                                   std::string configPath);
 
+        /**
+         * @brief Deconstruct a qrSwingLegController object.
+         */
         ~qrSwingLegController() = default;
 
         /**
          * @brief Quadratic interpolation function, used to generate polygon curve.
-         * @param phase
-         * @param start
+         * @param phase specifies the given phase in [0, 1] to compute the trajectory.
+         * @param start specifies the foot's position at the beginning of swing cycle.
          * @param mid
-         * @param end
+         * @param end specifies the foot's desired position at the end of swing cycle.
          * @return a float value with phase
          */
         float GenParabola(float phase, float start, float mid, float end);
@@ -55,40 +86,110 @@ namespace Quadruped {
                                                           Eigen::Matrix<float, 3, 1> startPos,
                                                           Eigen::Matrix<float, 3, 1> endPos);
 
+        /**
+         * @brief Reset the controller parameters.
+         */
         void Reset(float currentTime);
 
+        /**
+         * @brief Update the controller parameters.
+         */
         void Update(float currentTime);
 
-        /** @brief google's function for velocity mode control */
+
+        /** @brief Compute all motors' commands using this controller.
+         *  @return tuple<map, Matrix<float, 5, 1>> : 
+         *          return control ouputs (e.g. positions/torques) for all 12 motors.
+         */
         std::map<int, Eigen::Matrix<float, 5, 1>> GetAction();
 
+        /**
+         * @brief The desired linear velocity. This memeber variable appears in velocity mode.
+         */
         Eigen::Matrix<float, 3, 1> desiredSpeed; // appear in velocity mode usually.
+
+        /**
+         * @brief The desired angular velocity. This memeber variable appears in velocity mode.
+         */
         float desiredTwistingSpeed; // appear in velocity mode usually.
 
+        /**
+         * @brief the robot object to which the controller is associated.
+         */
         qrRobot *robot;
+
+        /**
+         * @brief Gait Generator object pointer.
+         */
         qrGaitGenerator *gaitGenerator;
+
+        /**
+         * @brief Robot estimator pointre.
+         */
         qrRobotEstimator *stateEstimator;
+
+        /**
+         * @brief Ground estimator pointer.
+         */
         qrGroundSurfaceEstimator *groundEstimator;
+
+        /**
+         * @brief Robot's foothold planner. Get desired COM pose when in walk locomotion.
+         */
         qrFootholdPlanner *footholdPlanner;
+
+        /**
+         * @brief The state of each leg.
+         */
         Eigen::Matrix<int, 4, 1> lastLegState;
+
+        /**
+         * @brief Desired robot's body height. 
+         */
         Eigen::Matrix<float, 3, 1> desiredHeight;
-        std::map<int, float> swigJointAngles;
+
+        /**
+         * @brief The joint's angles and motor velocities data structure. The first is joint angle,
+         *        the second is motor velocity and the third is the leg index.
+         */ 
         std::map<int, std::tuple<float, float, int>> swingJointAnglesVelocities;
-        Eigen::Matrix<float, 3, 4> phaseSwitchFootLocalPos; // foot positions in base frame when switch leg state
-        Eigen::Matrix<float, 3, 4> phaseSwitchFootGlobalPos; //foot positions in world frame when switch leg state
-        Eigen::Matrix<float, 3, 4> footHoldInWorldFrame; //footholds in world frame
-        Quat<float> controlFrameOrientationSource;
-        Vec3<float> controlFrameOriginSource;
-        Eigen::Matrix<float, 3, 4> phaseSwitchFootControlPos;
-        Eigen::Matrix<float, 3, 4> footHoldInControlFrame;
+
+        /**
+         * @brief Foot positions in the base frame when the leg state changes.
+         */
+        Eigen::Matrix<float, 3, 4> phaseSwitchFootLocalPos;
         
+        /**
+         * @brief Foot positions in the world frame when the leg state changes.
+         */
+        Eigen::Matrix<float, 3, 4> phaseSwitchFootGlobalPos;
+        
+        /**
+         * @brief the footholds in the world frame.
+         */
+        Eigen::Matrix<float, 3, 4> footHoldInWorldFrame;
+
+        /**
+         * @brief The trajectories of each leg.
+         */
         qrSwingFootTrajectory swingFootTrajectories[4];
-        // yaml config
+
+
+        /**
+         * @brief The config file path of swing_leg_controller.yaml.
+         */
         YAML::Node swingLegConfig;
-        // init pose in position mode
+
+        /**
+         * @brief the initial foot pose in position mode.
+         */
         std::vector<std::vector<float>> footInitPose;
+
+        /**
+         * @brief the foot offset.
+         */
         float footOffset;
     };
 } // namespace Quadruped
 
-#endif //ASCEND_QUADRUPED_CPP_RAIBERT_SWING_LEG_CONTROLLER_H
+#endif //QR_SWING_LEG_CONTROLLER_H
--- src/quadruped/include/quadruped/mpc_controller/qr_torque_stance_leg_controller.h
+++ src/quadruped/include/quadruped/mpc_controller/qr_torque_stance_leg_controller.h
@@ -1,14 +1,29 @@
-/*
-* Copyright (c) Huawei Technologies Co., Ltd. 2021-2022. All rights reserved.
-* Description: Stance controller for stance foot.
-* Author: Zang Yaohua & Zhao Yao
-* Create: 2021-10-25
-* Notes: xx
-* Modify: init the file. @ Zang Yaohua
-*/
+// The MIT License
 
-#ifndef ASCEND_QUADRUPED_CPP_TORQUE_STANCE_LEG_CONTROLLER_H
-#define ASCEND_QUADRUPED_CPP_TORQUE_STANCE_LEG_CONTROLLER_H
+// Copyright (c) 2022 
+// Robot Motion and Vision Laboratory at East China Normal University
+// Contact: tophill.robotics@gmail.com
+
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE. 
+
+#ifndef QR_STANCE_LEG_CONTROLLER_H
+#define QR_STANCE_LEG_CONTROLLER_H
 
 #include "utils/se3.h"
 #include "robots/qr_robot.h"
@@ -25,22 +40,46 @@ namespace Quadruped {
      */
     class qrStanceLegController {
     public:
-        qrStanceLegController(qrRobot *robot,
-                                  qrGaitGenerator *gaitGenerator,
-                                  qrRobotEstimator *robotVelocityEstimator,
-                                  qrGroundSurfaceEstimator *groundEstimatorIn,
-                                  qrComPlanner *comPlanner,
-                                  qrPosePlanner *posePlanner,
-                                  qrFootholdPlanner *footholdPlanner,
-                                  Eigen::Matrix<float, 3, 1> desired_speed,
-                                  float desiredTwistingSpeed,
-                                  float desiredBodyHeight,
-                                  int numLegs,
-                                  std::string configFilepath,
-                                  std::vector<float> frictionCoeffs = {0.45, 0.45, 0.45, 0.45});
 
+        /**
+         * @brief Constructor of qrStanceLegController using given many object pointers and attributes.
+         * @param robot The robot object pointer.
+         * @param gaitGenerator The gait generator object pointer.
+         * @param robotVelocityEstimator The robot estimator object pointer.
+         * @param groundEstimatorIn The ground estimator object pointer.
+         * @param comPlanner The COM planner object pointer.
+         * @param posePlanner The pose planner object pointer.
+         * @param footholdPlanner The foothold planner object pointer.
+         * @param desired_speed The robot desired speed in velocity control.
+         * @param desiredTwistingSpeed The robot desired twisting speed in velocity control.
+         * @param desiredBodyHeight The robot desired body height.
+         * @param numLegs The number of leg.
+         * @param configFilepath The stance leg config file path.
+         * @param frictionCoeffs The coefficients of friction.
+         */
+        qrStanceLegController(qrRobot *robot,
+                                qrGaitGenerator *gaitGenerator,
+                                qrRobotEstimator *robotVelocityEstimator,
+                                qrGroundSurfaceEstimator *groundEstimatorIn,
+                                qrComPlanner *comPlanner,
+                                qrPosePlanner *posePlanner,
+                                qrFootholdPlanner *footholdPlanner,
+                                Eigen::Matrix<float, 3, 1> desired_speed,
+                                float desiredTwistingSpeed,
+                                float desiredBodyHeight,
+                                int numLegs,
+                                std::string configFilepath,
+                                std::vector<float> frictionCoeffs = {0.45, 0.45, 0.45, 0.45});
+
+        /**
+         * @brief Default desconstructor of qrStanceLegController
+         */
         virtual ~qrStanceLegController() = default;
 
+        /**
+         * @brief Reset the parameters of the qrStanceLegController.
+         * @param currentTime Current run time
+         */
         void Reset(float currentTime);
 
         /**
@@ -51,36 +90,130 @@ namespace Quadruped {
          */
         void UpdateFRatio(Vec4<bool> &contacts, int &N, float &normalizedPhase);
 
+        /**
+         * @brief Update the parameters of the qrStanceLegController.
+         * @param currentTime Current run time
+         */
         void Update(float currentTime);
 
+        /** 
+         * @brief Compute all motors' commands via controllers.
+         *  @return tuple<map, Matrix<3,4>> : 
+         *          return control ouputs (e.g. positions/torques) for all (12) motors.
+         */
         virtual std::tuple<std::map<int, qrMotorCommand>, Eigen::Matrix<float, 3, 4>> GetAction();
 
+        /**
+         * @brief The robot object pointer.
+         */
         qrRobot *robot;
+
+        /**
+         * @brief Gait Generator object pointer.
+         */
         qrGaitGenerator *gaitGenerator;
+
+        /**
+         * @brief Robot estimator pointre. Get the estimated velocity.
+         */
         qrRobotEstimator *robotEstimator;
+
+        /**
+         * @brief Ground estimator pointer.
+         */
         qrGroundSurfaceEstimator *groundEstimator;
+
+        /**
+         * @brief The center of mass adjuster pointer. Get the position of COM in base frame
+         *        in position locomotion.
+         */
         qrComPlanner *comPlanner;
-        qrPosePlanner *posePlanner;                          
+
+        /**
+         * @brief Robot pose planner obejct pointer. Get the intermediate base pose.
+         */
+        qrPosePlanner *posePlanner;
+
+        /**
+         * @brief Robot's foothold planner. Get desired COM pose when in walk locomotion.
+         */
         qrFootholdPlanner *footholdPlanner;
+
+        /**
+         * @brief Desired speed of the robot in walk or position locomotion.
+         */
         Eigen::Matrix<float, 3, 1> desiredSpeed = {0., 0., 0.};
+
+        /**
+         * @brief The speed message of twist command given by gamepad.
+         */
         float desiredTwistingSpeed = 0.;
+
+        /**
+         * @brief Desired robot's body height. Overwrite in the class constructor by robot->bodyHeight
+         */
         float desiredBodyHeight = 0.45; //overwrite in the class constructor by robot->bodyHeight
+        
+        /**
+         * @brief The number of legs.
+         */
         int numLegs = 4;
+
+        /**
+         * @brief The coefficients of friction.
+         */
         std::vector<float> frictionCoeffs = {0.45, 0.45, 0.45, 0.45};
+
+        /**
+         * @brief File path of stance_leg_controller.yaml.
+         */
         std::string configFilepath;
 
+        /**
+         * @brief The dimension of force.
+         */
         int force_dim;
+
+        /**
+         * @brief The parameter KP in stance_leg_controller.yaml.
+         */
         Eigen::Matrix<float, 6, 1> KP;
+
+        /**
+         * @brief The parameter KD in stance_leg_controller.yaml.
+         */
         Eigen::Matrix<float, 6, 1> KD;
+
+        /**
+         * @brief The parameter maxDdq in stance_leg_controller.yaml.
+         */
         Eigen::Matrix<float, 6, 1> maxDdq;
+
+        /**
+         * @brief The parameter minDdq in stance_leg_controller.yaml.
+         */
         Eigen::Matrix<float, 6, 1> minDdq;
+
+        /**
+         * @brief The parameter accWeight in stance_leg_controller.yaml.
+         */
         Eigen::Matrix<float, 6, 1> accWeight;
+
+        /**
+         * @brief The minimum ratio.
+         */
         Vec4<float> fMinRatio; // the minimum ratio
+
+        /**
+         * @brief The maximum ratio.
+         */
         Vec4<float> fMaxRatio; // the maximum ratio
-        
+
+        /**
+         * @brief Current time.
+         */
         float currentTime;
-        long long count = 0;
     };
 } // namespace Quadruped
 
-#endif //ASCEND_QUADRUPED_CPP_TORQUE_STANCE_LEG_CONTROLLER_H
+#endif //QR_STANCE_LEG_CONTROLLER_H
