8ec36c97a809ffcffb8b79ef10311e2f0417daa7
Gong, Ke <gk.kingj@gmail.com>
2022-09-02 21:42:19 +0800

    fix keep stand

--- demo/a1sim/demo_helloworld/demo_helloworld.cpp
+++ demo/a1sim/demo_helloworld/demo_helloworld.cpp
@@ -28,7 +28,9 @@ int main(int argc, char **argv)
     qrRobot *quadruped = new qrRobotA1Sim(nh, pathToNode + "/config/a1_sim.yaml");
     quadruped->ReceiveObservation();
 
-    // the quadruped robot stands up
+    /* the quadruped robot stands up.
+    (the parameters are robot, the time that stand up need, the total time before excuting other action and time step)
+    */
     Action::StandUp(quadruped, 3.f, 5.f, 0.001f);
 
     float startTime = quadruped->GetTimeSinceReset();
@@ -36,12 +38,11 @@ int main(int argc, char **argv)
     float startTimeWall = startTime;
 
     // keep ROS running to let the robot stand.
-    while (ros::ok() && currentTime - startTime < MAX_TIME_SECONDS) {
-        /*
-          Put control logic here.
-        */
-    }
-    
+    // 20.f means that the robot keep stands for 20.0 seconds and 0.001 is the time step.
+    std::cout << "---------keep quadruped robot standing for 20.0 seconds---------" << std::endl;
+    Action::KeepStand(quadruped, 20.f, 0.001f);
+    std::cout << "---------quadruped robot sitting down---------" << std::endl;
+    Action::SitDown(quadruped, 3.f, 0.001f);
     ROS_INFO("Time is up, end now.");
     ros::shutdown();
     return 0;
