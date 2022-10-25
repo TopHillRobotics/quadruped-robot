e4680c2318ad86caed870378a6e625b86ae91dc8
Leonhard Grey <603216573@qq.com>
2022-10-26 03:54:45 +0800

    Improve some params of slam

--- navigation/slam/config/cartographer_config/trajectory_builder_2d.lua
+++ navigation/slam/config/cartographer_config/trajectory_builder_2d.lua
@@ -14,38 +14,38 @@
 
 TRAJECTORY_BUILDER_2D = {
     use_imu_data = true,
-    min_range = 0.,
-    max_range = 30.,
+    min_range = 0,
+    max_range = 30,
     min_z = -0.8,
-    max_z = 2.,
-    missing_data_ray_length = 5.,
+    max_z = 2,
+    missing_data_ray_length = 5,
     num_accumulated_range_data = 1,
     voxel_filter_size = 0.025,
   
     adaptive_voxel_filter = {
       max_length = 0.5,
       min_num_points = 200,
-      max_range = 50.,
+      max_range = 50,
     },
   
     loop_closure_adaptive_voxel_filter = {
       max_length = 0.9,
       min_num_points = 100,
-      max_range = 50.,
+      max_range = 50,
     },
   
     use_online_correlative_scan_matching = false,
     real_time_correlative_scan_matcher = {
       linear_search_window = 0.1,
-      angular_search_window = math.rad(20.),
+      angular_search_window = math.rad(20),
       translation_delta_cost_weight = 1e-1,
       rotation_delta_cost_weight = 1e-1,
     },
   
     ceres_scan_matcher = {
-      occupied_space_weight = 1.,
-      translation_weight = 10.,
-      rotation_weight = 40.,
+      occupied_space_weight = 1,
+      translation_weight = 10,
+      rotation_weight = 40,
       ceres_solver_options = {
         use_nonmonotonic_steps = false,
         max_num_iterations = 20,
@@ -54,12 +54,12 @@ TRAJECTORY_BUILDER_2D = {
     },
   
     motion_filter = {
-      max_time_seconds = 5.,
+      max_time_seconds = 5,
       max_distance_meters = 0.2,
-      max_angle_radians = math.rad(1.),
+      max_angle_radians = math.rad(1),
     },
   
-    imu_gravity_time_constant = 10.,
+    imu_gravity_time_constant = 10,
   
     submaps = {
       num_range_data = 90,
