5295ba1b4f3538d2f39bc44e9248de4dfc9097b1
ymzzz <757922306@qq.com>
2022-11-07 13:20:48 +0800

    add contact&trajectory visualization plugin

--- simulation/qr_gazebo/plugin/draw_force_plugin.cc
+++ simulation/qr_gazebo/plugin/draw_force_plugin.cc
@@ -63,9 +63,9 @@ namespace gazebo
 
         void GetForceCallback(const geometry_msgs::WrenchStamped & msg)
         {
-            Fx = msg.wrench.force.x/20.0;
-            Fy = msg.wrench.force.y/20.0;
-            Fz = msg.wrench.force.z/20.0;
+            Fx = msg.wrench.force.x/2.0;
+            Fy = msg.wrench.force.y/2.0;
+            Fz = msg.wrench.force.z/2.0;
             // Fx = msg.wrench.force.x;
             // Fy = msg.wrench.force.y;
             // Fz = msg.wrench.force.z;
